b435c39a0c35af6d91cf5292204dbe1a88c5e601
IvanNajdovski <ivan_najdovski@hotmail.com>
2018-09-09 17:22:20 +0200

    alert message

--- js/index.js
+++ js/index.js
@@ -50,6 +50,7 @@ $(document).ready(function() {
                 counter = 0;
                 if(brojka > 17){
                     $(".div").css("border","none")
+                    alert("a dali si cute?");
                 }
                 }, 1000);
             } else {
